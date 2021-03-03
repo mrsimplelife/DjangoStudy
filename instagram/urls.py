f4b60fc034d6906eb91c32fc94a6bba5539694e6
youme <mrsimplelife@naver.com>
2021-03-04 06:27:42 +0900

    Add post_detail view

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,8 +1,10 @@
 
 from instagram import views
-from django.urls import path
+from django.urls import path, re_path
 
 
 urlpatterns = [
-    path('', views.post_list, name='post_list')
+    path('', views.post_list, name='post_list'),
+    path('<int:pk>/', views.post_detail, name='post_detail'),
+    # re_path(r'(?P<pk>\d+)/$', views.post_detail,)
 ]
--- instagram/views.py
+++ instagram/views.py
@@ -1,14 +1,21 @@
+from django.views.generic import ListView
+from django.http import HttpRequest, HttpResponse
+# from django.shortcuts import render
 from instagram.models import Post
-from django.http import HttpRequest
-from django.shortcuts import render
+
+post_list = ListView.as_view(model=Post)
+# def post_list(request: HttpRequest) -> HttpResponse:
+#     q = request.GET.get('q', '')
+#     qs = Post.objects.all()
+#     if q:
+#         qs = qs.filter(message__icontains=q)
+#     return render(request, 'instagram/post_list.html', {
+#         'post_list': qs,
+#         'q': q
+#     })
 
 
-def post_list(request: HttpRequest):
-    q = request.GET.get('q', '')
-    qs = Post.objects.all()
-    if q:
-        qs = qs.filter(message__icontains=q)
-    return render(request, 'instagram/post_list.html', {
-        'post_list': qs,
-        'q': q
-    })
+def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    response = HttpResponse()
+    response.write(f'{pk}')
+    return response
