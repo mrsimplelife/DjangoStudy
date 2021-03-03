b28248b44ffd74f5f085928cf995f188446d9a6a
youme <mrsimplelife@naver.com>
2021-03-03 14:33:00 +0900

    Add insta admin

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,3 +1,20 @@
+from re import S
+from instagram.models import Post
 from django.contrib import admin
 
 # Register your models here.
+# admin.site.register(Post)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'message', 'message_length',
+                    'is_public', 'created_at', 'updated_at', ]
+    list_display_links = ['message']
+    list_filter = ['is_public', 'created_at', ]
+    search_fields = ['message']
+
+    def message_length(self, instance):
+        return f"{len(instance.message)} 글자"
+    message_length.short_description = "메세지 글자수"
+# admin.site.register(Post, PostAdmin)
--- instagram/models.py
+++ instagram/models.py
@@ -3,5 +3,14 @@ from django.db import models
 
 class Post(models.Model):
     message = models.TextField(blank=True)
+    is_public = models.BooleanField(default=False, verbose_name='공개')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        # return f"Post object({self.pk})"
+        return self.message
+
+    # def message_length(self):
+    #     return len(self.message)
+    # message_length.short_description = "메세지 글자수"
